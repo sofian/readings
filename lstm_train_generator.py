3c6ac896ddd5b21c4a787f5184336c3f7e5d61b5
Tats <info@sofianaudry.com>
2017-01-26 16:49:41 -0500

    Added possibility of restarting training at certain epoch.

--- lstm_train_generator.py
+++ lstm_train_generator.py
@@ -14,6 +14,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("text_file", type=str, help="The file containing the original text")
 parser.add_argument("-n", "--n-hidden", type=int, default=256, help="Number of hidden units")
 parser.add_argument("-s", "--sequence-length", type=int, default=100, help="Sequence length")
+parser.add_argument("-m", "--model-file", type=str, default="", help="Model file to load (in order to restart from a certain point)")
+parser.add_argument("-i", "--initial-epoch", type=int, default=0, help="Epoch at which to start training (useful for resuming previous training)")
 
 args = parser.parse_args()
 
@@ -55,6 +57,11 @@ model = Sequential()
 model.add(LSTM(args.n_hidden, input_shape=(X.shape[1], X.shape[2])))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
+
+if (args.model_file):
+  print "Loading weights from: " + args.model_file
+  model.load_weights(args.model_file)
+
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 
@@ -63,7 +70,8 @@ filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-model.fit(X, y, nb_epoch=20, batch_size=128, callbacks=callbacks_list)
+
+model.fit(X, y, nb_epoch=20, batch_size=128, callbacks=callbacks_list, initial_epoch=args.initial_epoch)
 
 
 
