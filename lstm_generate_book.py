5c86ef807c860a45383060d10f5c5423c3c04d6d
Tats <info@sofianaudry.com>
2017-04-26 14:24:57 -0400

    Integrate "special" option and smooth transitions between epochs.

--- lstm_generate_book.py
+++ lstm_generate_book.py
@@ -9,10 +9,12 @@ parser.add_argument("-n", "--n-hidden", type=int, default=256, help="Number of h
 parser.add_argument("-l", "--n-layers", type=int, default=1, help="Number of layers")
 parser.add_argument("-e", "--n-epochs", type=int, default=20, help="Number of epochs")
 parser.add_argument("-s", "--sequence-length", type=int, default=100, help="Sequence length")
-parser.add_argument("-S", "--sampling_mode", type=str, default="argmax", choices=["argmax", "softmax", "special"], help="Sampling policy")
+parser.add_argument("-S", "--sampling_mode", type=str, default="argmax", choices=["argmax", "softmax"], help="Sampling policy")
 parser.add_argument("-N", "--n-words", type=int, default=1000, help="Number of words to generate per epoch/chapter")
 parser.add_argument("-T", "--temperature", type=float, default=1, help="Temperature argument [0, +inf] (for softmax sampling) (higher: more uniform, lower: more greedy")
 parser.add_argument("-E", "--temperature-end", type=float, default=-1, help="Temperature end value (if <= 0 : don't change)")
+parser.add_argument("-b", "--n-best", type=int, default=0, help="Number of best choices from which to pick (to avoid too unlikely outcomes)")
+parser.add_argument("-t", "--transition-factor", type=float, default=0, help="Portion of words in each step that will smoothly transition from one model to the next (in [0, 1])")
 
 args = parser.parse_args()
 
@@ -25,6 +27,23 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
+
+
+def create_model(args):
+	# define the LSTM model
+	model = Sequential()
+	model.add(LSTM(args.n_hidden, input_shape=(X.shape[1], X.shape[2]), return_sequences=(args.n_layers > 1)))
+	model.add(Dropout(0.2))
+	for l in range(1, args.n_layers):
+		model.add(LSTM(args.n_hidden, return_sequences=(l < args.n_layers-1)))
+		model.add(Dropout(0.2))
+	model.add(Dense(y.shape[1], activation='softmax'))
+	return model
+
+def load_model(model, model_file):
+	# load the network weights
+	model.load_weights(model_file)
+	model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 
 # load ascii text and covert to lowercase
@@ -74,15 +93,17 @@ X = X / float(n_vocab)
 # one hot encode the output variable
 y = np_utils.to_categorical(dataY)
 
+n_words = args.n_words
+transition_factor = args.transition_factor
+transition_n_words = int(n_words * transition_factor)
+transition_start_word = n_words - transition_n_words
 
-# define the LSTM model
-model = Sequential()
-model.add(LSTM(args.n_hidden, input_shape=(X.shape[1], X.shape[2]), return_sequences=(args.n_layers > 1)))
-model.add(Dropout(0.2))
-for l in range(1, args.n_layers):
-	model.add(LSTM(args.n_hidden, return_sequences=(l < args.n_layers-1)))
-	model.add(Dropout(0.2))
-model.add(Dense(y.shape[1], activation='softmax'))
+# Create model
+model = create_model(args)
+if transition_factor > 0:
+  model_next = create_model(args)
+else:
+  model_next = None
 
 print model.summary()
 
@@ -94,6 +115,8 @@ print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
 
 temperature = args.temperature
 
+n_best = args.n_best
+
 # Run through all epochs, outputing text
 for e in range(n_epochs):
 	if (args.temperature_end > 0):
@@ -101,26 +124,36 @@ for e in range(n_epochs):
 		temperature = args.temperature + (float(e)/(args.n_epochs-1)) * (args.temperature_end - args.temperature)
 
 	model_file = model_files[e]
-	print "Generating epoch # {epoch} (temperature={temp}) using file {filename}".format(epoch=e,temp=temperature,filename=model_file)
+	print "Generating epoch/step # {epoch} (temperature={temp}) using file {filename}".format(epoch=e,temp=temperature,filename=model_file)
 	# load the network weights
 	model.load_weights(model_file)
 	model.compile(loss='categorical_crossentropy', optimizer='adam')
 	#output_file.write("\n\nEPOCH {epoch}\n\n".format(epoch=e))
+	
+	if transition_factor > 0 and e < n_epochs-1:
+		model_next.load_weights(model_files[e+1])
+		model_next.compile(loss='categorical_crossentropy', optimizer='adam')
 
 	# generate characters
-	for i in range(args.n_words):
+	for i in range(n_words):
 		x = numpy.reshape(pattern, (1, len(pattern), 1))
 		x = x / float(n_vocab)
-		prediction = model.predict(x, verbose=0)
+		
+		# run prediction of model
+		prediction = model.predict(x, verbose=0).squeeze()
+		
+		if transition_factor > 0 and i >= transition_start_word:
+		  prediction_next = model_next.predict(x, verbose=0).squeeze()
+		  mix_factor = (n_words - i) / float(transition_n_words)
+		  prediction = mix_factor*prediction + (1-mix_factor)*prediction_next
 
 		# argmax
 		if (args.sampling_mode == "argmax"):
-			index = numpy.argmax(prediction.squeeze())
+			index = numpy.argmax(prediction)
 
 		# random
-		else:
+		elif (args.sampling_mode == "softmax"):
 			# Source: https://gist.github.com/alceufc/f3fd0cd7d9efb120195c
-			prediction = prediction.squeeze()
 			if (temperature != 1):
 				prediction = numpy.power(prediction, 1./temperature)
 				prediction /= prediction.sum(0)
@@ -128,19 +161,19 @@ for e in range(n_epochs):
 			#print "[" + args.sampling_mode +"]"
 			#print (args.sampling_mode == "softmax")
 
-			if (args.sampling_mode == "softmax"):
+			if n_best == 0:
 				#print "using softmax"
 				index = numpy.asscalar(numpy.random.choice(numpy.arange(n_vocab), 1, p=prediction))
 
 			# special
 			else:
 				prediction = numpy.asarray(prediction)
-				max_indices = prediction.argsort()[-3:][::-1]
+				max_indices = prediction.argsort()[-n_best:][::-1]
 				max_indices_weights = [ prediction[m] for m in max_indices ]
 				max_indices_sum = numpy.sum(max_indices_weights)
 				max_indices_weights = [ prediction[m]/max_indices_sum for m in max_indices ]
 				index = numpy.asscalar(numpy.random.choice(numpy.array(max_indices), 1, p=numpy.array(max_indices_weights)))
-
+		
 		result = int_to_char[index]
 		seq_in = [int_to_char[value] for value in pattern]
 		output_file.write(result)
@@ -148,3 +181,5 @@ for e in range(n_epochs):
 		pattern = pattern[1:len(pattern)]
 
 	output_file.flush()
+	
+
