1500726df4e6b6a4df024a60373bd30c58075cff
Sofian Audry <info@sofianaudry.com>
2018-08-23 00:05:08 -0400

    Generate script with all list

--- generate_fds.sh
+++ generate_fds.sh
@@ -7,7 +7,7 @@ raw_output="fts_${timestamp}.txt"
 final_output="fts_${timestamp}_final.txt"
 
 # Run generation.
-python lstm_generate_book.py -em 5 -l 2 -n 200 -N 1833 -S softmax -T 0.05 -E 1 -b 5 -t 0.2 -D results/wuthering-em5-nhu200 data/wuthering.txt results/wuthering-em5-nhu200/list_098 results/${raw_output}
+python lstm_generate_book.py -em 5 -l 2 -n 200 -N 1833 -S softmax -T 0.05 -E 1 -b 5 -t 0.2 -D results/wuthering-em5-nhu200 data/wuthering.txt results/wuthering-em5-nhu200/list results/${raw_output}
 
 # Post-process.
 python postprocess_generated_book.py -N 1833 results/${raw_output} data/wuthering.txt results/${final_output}
